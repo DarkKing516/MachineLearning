e5673f273f82a98672f01ed63376975c5fff4a04
unknown <Sena@MEDAPRCSGFSD729.FormacionSena.RED>
2024-04-23 13:13:54 -0500

    si

--- archivos_planos/plano.py
+++ archivos_planos/plano.py
@@ -5,4 +5,21 @@ def escribirPlano(linea):
         archivo.write(linea + "\n")
         
         
-escribirPlano("Hola Mundo")
+escribirPlano("Hola Mundo,")
+
+def añadirPlano(linea):
+    with open("./datos.sen", "a") as archivo:
+        archivo.write(linea + "\n")
+        
+        
+añadirPlano("Hello World,")
+
+def LeerPLano():
+    with open("./datos.sen", "r") as plano:
+        leer = csv.reader(plano, delimiter= ",")
+        for linea in leer:
+            for nombre in linea:
+                print(nombre)
+
+# añadirPlano("ola, si, oa")
+LeerPLano()
